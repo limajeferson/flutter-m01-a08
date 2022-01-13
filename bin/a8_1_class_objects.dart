1dc14b565e69b089cc888ef026b17e0c640a2222
limajeferson <jefersonlima@outlook.com>
2022-01-13 19:09:10 -0300

    20220112_pasta_a08

---
+++ bin/a8_1_class_objects.dart
@@ -0,0 +1,17 @@
+//* Sintaxe de uma Classe
+
+  //* Membros: variáveis -> fields/campos (propriedades)
+
+  //* Construtor: Classe -> construtor -> Objeto
+
+  //* Membros: funções -> métodos (comportamentos)
+
+void main() {
+  //* Istanciar um Objeto
+
+  //* Acessar membros
+
+  //* Checar qual a classe/tipo de um objeto
+
+  //? *Tudo é um objeto*
+}
---
+++ test/flutter_m01_a08_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_m01_a08/flutter_m01_a08.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
