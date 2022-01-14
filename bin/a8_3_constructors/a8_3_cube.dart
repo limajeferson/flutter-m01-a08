77288b4ab0c5112506a9ff891b5cdda23744a3fb
limajeferson <jefersonlima@outlook.com>
2022-01-14 15:09:10 -0300

    aula03

--- bin/a8_3_constructors/a8_3_cube.dart
+++ bin/a8_3_constructors/a8_3_cube.dart
@@ -1,12 +1,29 @@
+import 'dart:math';
+
 class Cube {
   //* Fields
+  final double side;
+  final double volume;
 
   //* Default
+  // Cube(this.side);
 
   //* Parâmetros nomeados - Initializer List
+  // Cube({required this.side});
+  // Cube({required this.side}) {
+  //   print('Cubo foi inicializado');
+  // }
+  // Cube({required this.side}) : volume = side * side * side {
+  //   print('Cubo foi inicializado: $side');
+  // }
 
   //* Named, default, sobreescrito, redirecionar contrutor
+  // Cube.withVolume({required this.volume})
+  //     : side = ;
+  // Cube.withVolume({required double volume})
+  //     : this(side: pow(volume, 1 / 3).toDouble());
+  const Cube.withVolume({required this.volume, required this.side});
 
   //* Const - hash code e ==
-
+  const Cube({required this.side}) : volume = side * side * side;
 }
--- bin/a8_3_constructors/a8_3_main.dart
+++ bin/a8_3_constructors/a8_3_main.dart
@@ -1,1 +1,11 @@
-void main() {}
+import 'a8_3_cube.dart';
+
+void main() {
+  // final myCube = Cube();
+  // final myCube = Cube(side: 2);
+  final myCube = const Cube.withVolume(volume: 8, side: 2);
+  final myCube2 = const Cube(side: 2);
+  print(myCube == myCube2);
+  print(myCube.hashCode);
+  print(myCube2.hashCode);
+}
